3b05c200910fc232b3b85c9c83f7f9a2809a424c
SamsonSuperMax <mschmidleitner@hotmail.com>
2025-06-25 21:53:56 +0200

    Add files via upload

---
+++ svw_github_project/src/main.jsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>SV Windischgarsten App</h1>
+      <p>Login & Module folgen...</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
